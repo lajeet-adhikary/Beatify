d92ab17e789d28adfe8665dfc9288ec4fda2b13f
Lajeet Adhikary <lajeet@apna.co>
2024-06-16 08:38:51 +0530

    pushed manifest file + some extras

--- app/src/main/java/com/lajeet/beatify/introAndAuth/presentation/composables/auth/AuthContainer.kt
+++ app/src/main/java/com/lajeet/beatify/introAndAuth/presentation/composables/auth/AuthContainer.kt
@@ -3,11 +3,11 @@ package com.lajeet.beatify.introAndAuth.presentation.composables.auth
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-
 
 @Preview
 @Composable
@@ -20,7 +20,7 @@ fun AuthContainer() {
     Column(
         modifier = Modifier.fillMaxWidth()
     ) {
-        Text(text = "Google")
-        Text(text = "Facebook")
+        TextField(value = "email", onValueChange = {})
+        TextField(value = "password", onValueChange = {})
     }
 }
